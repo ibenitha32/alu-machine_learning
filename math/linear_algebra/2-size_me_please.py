3806e180204bcd2f207d88804a9729409da4b34b
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2025-03-04 22:22:11 +0200

    Update 2-size_me_please.py
    done?

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-""" Calculates the shape of a matrix
+
+"""
+ arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+ this module will return the size of the array
 """
 
+
 def matrix_shape(matrix):
-    """ Calculates the shape of a matrix """
+    """
+    matrix: list of list
+    return: list of integers
+    """
     shape = []
-    while isinstance(matrix, list):
+    while type(matrix) == list:
         shape.append(len(matrix))
         matrix = matrix[0]
     return shape
+
+
+if __name__ == "__main__":
+    matrix_shape_function = __import__('2-size_me_please').matrix_shape
